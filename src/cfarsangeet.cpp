51b691fabfab369598d5a8b66783434aa8c2d962
sangeet259 <mail2sangeetmishra@gmail.com>
2017-05-24 11:36:16 +0530

    almost done2

--- src/cfarsangeet.cpp
+++ src/cfarsangeet.cpp
@@ -7,9 +7,12 @@
 #include "opencv2/opencv.hpp"
 #include <fstream>
 
+
+
 using namespace cv;
 using namespace std;
-ofstream outf("Sample.txt");
+
+ofstream outf("output.txt");
 
 bool isGuardCell(int i,int j , int y, int x)
 {
@@ -19,12 +22,14 @@ bool isGuardCell(int i,int j , int y, int x)
 
 Mat cfar(Mat img_hsv)
 {
+  outf<<"inside the cfar function"<<endl;
   Mat img_grayscale(img_hsv.rows, img_hsv.cols, CV_8UC1, Scalar(255));
+  outf<<"img_grayscale declared"<<endl;
 
   //iterate over all the pixels of this- hsv image
-  for (int y = 0; y < img_hsv.rows; ++y) {
+  for (int y = 8; y < img_hsv.rows-8; ++y) {
 
-    for (int x = 0; x < img_hsv.cols; ++x) {
+    for (int x = 8; x < img_hsv.cols-8; ++x) {
 
       // Get value current channel and current pixel
       const cv::Vec3b& s = img_hsv.at<cv::Vec3b>(y, x);
@@ -35,12 +40,17 @@ Mat cfar(Mat img_hsv)
       //first the 'y'
       float sum_value_channel_train=0;
       float sum_value_channel_guard=0;
-
-      for (int j =y-3 ; j <= y+3; y++)
+      outf<<"checkpoint 1"<<endl;
+      for (int j =y-3 ; j <= y+3; j++)
       {
+        outf<<"j is "<<j<<endl;
+        outf<<"traversing rows"<<endl;
         //then the 'x'
-        for (int i =x-3 ; i <= x+3; x++)
+        for (int i =x-3 ; i <= x+3; i++)
         {
+          outf<< "i is "<<i<<endl; 
+          outf<<"traversing columns"<<endl;
+          outf<<"checkpoint 2"<<endl;
           //take the value channel of this pixel and add to the local scope around this pixel
           const cv::Vec3b& r = img_hsv.at<cv::Vec3b>(y, x);
           unsigned int pxl_val_inside = (unsigned int)r.val[2];
@@ -61,6 +71,7 @@ Mat cfar(Mat img_hsv)
           //Compare and excecute
             if (pxl_val_current>avg_value_train)
               //color a pixel black
+              
               img_grayscale.at<unsigned char>(y,x)=0;
 
 
@@ -70,24 +81,33 @@ Mat cfar(Mat img_hsv)
     }
   }
 
+  return img_grayscale;
+
 
 }
 
 //Lets Clear some trash :)
 
 int main() {
-outf<<"Inside main";
+  int frameno=1;
+  
+  outf << "Initialising" <<endl;
   //Open the video
     VideoCapture cap("buoy.avi");
     // Check if opened properly
+    outf<<"Initialised"<<endl;
     if(!cap.isOpened()){
       cout<<"Can't open video file.";
       return -1;
     }
     Mat frame, hsv;
     while(cap.read(frame)) {
+      outf<<"Reading the frame #"<<frameno++<<endl;
   
     cvtColor(frame, hsv, COLOR_BGR2HSV);
+    cout<<"Okay\n";
+    outf<<"Into the cfar function"<<endl;
+    outf<<"Okay"<<endl;
     Mat image = cfar(hsv);
       /*cvtColor(image, grey, COLOR_BGR2GRAY);*/
       /*dilate(grey, grey, 0, Point(-1, -1), 5, 1, 1);*/
